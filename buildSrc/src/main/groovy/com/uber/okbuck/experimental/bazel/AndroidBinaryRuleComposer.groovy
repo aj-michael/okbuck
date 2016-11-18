3d36d857ff0d481a2157bc096c74c2c9d67d6459
Adam Michael <adam@ajmichael.net>
2016-11-17 21:53:23 -0500

    This is everything.

---
+++ buildSrc/src/main/groovy/com/uber/okbuck/experimental/bazel/AndroidBinaryRuleComposer.groovy
@@ -0,0 +1,15 @@
+package com.uber.okbuck.experimental.bazel
+
+import com.uber.okbuck.composer.AndroidBuckRuleComposer
+import com.uber.okbuck.core.model.AndroidAppTarget
+
+final class AndroidBinaryRuleComposer extends AndroidBuckRuleComposer {
+    static AndroidBinaryRule compose(AndroidAppTarget target) {
+        return new AndroidBinaryRule(
+                bin(target),
+                target.getPackage(),
+                ":${src(target)}",
+                target.manifest,
+                target.placeholders)
+    }
+}
